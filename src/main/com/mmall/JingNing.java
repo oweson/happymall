dbe380bf0edbe5b34d520cc450e759e8c9107b4f
oweson <570347720@qq.com>
2019-03-03 22:31:39 +0800

    come on;

---
+++ src/main/com/mmall/JingNing.java
@@ -0,0 +1,12 @@
+package com.mmall;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/3/3 0003 22:31
+ */
+public class JingNing {
+    public static void main(String[] args) {
+        System.out.println("南京");
+    }
+}
